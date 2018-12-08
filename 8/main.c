d7496dbc7e3a91d09e7996156b7051ff458a1cb6
lucy <lucy@luz.lu>
2018-12-08 23:38:17 +0100

    actually the worst

--- 8/main.c
+++ 8/main.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int p1(int *r, int *l, int ii) {
-	int c = l[ii++], m = l[ii++];
-	for (int i = 0; i < c; i++) ii = p1(r, l, ii);
-	for (int i = 0; i < m; i++) *r += l[ii++];
-	return ii;
-}
-
-int p2(int *r, int *l, int ii) {
+int p(int *r1, int *r2, int *l, int ii) {
 	int c = l[ii++], m = l[ii++], *cc = c ? calloc(c, sizeof(int)) : 0;
-	for (int i = 0; i < c; i++) ii = p2(&cc[i], l, ii);
-	for (int i = 0, j; i < m; i++)
-		if ((j = l[ii++]), !c) *r += j;
-		else if (j && j <= c) *r += cc[j-1];
+	for (int i = 0; i < c; i++) ii = p(r1, &cc[i], l, ii);
+	for (int i = 0, j; i < m; i++) {
+		j = l[ii++], *r1 += j;
+		if (!c) *r2 += j;
+		else if (j && j <= c) *r2 += cc[j-1];
+	}
 	free(cc);
 	return ii;
 }
@@ -21,7 +16,6 @@ int p2(int *r, int *l, int ii) {
 int main(void) {
 	int l[1<<15] = {0}, len = 0, r1 = 0, r2 = 0;
 	while (scanf("%d", &l[len++]) == 1);
-	p1(&r1, l, 0);
-	p2(&r2, l, 0);
+	p(&r1, &r2, l, 0);
 	printf("%d\n%d\n", r1, r2);
 }
