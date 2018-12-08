55d1ac85153a944500bf92af9429635f19f7b08a
lucy <lucy@luz.lu>
2018-12-08 19:34:45 +0100

    day 8

---
+++ 8/main.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct t { int mlen, clen, *m; struct t *c; };
+
+int tree(struct t *n, int *input, int ii, int len) {
+	if (ii >= len) return 0;
+	n->clen = input[ii++], n->mlen = input[ii++];
+	n->c = calloc(n->clen, sizeof(*n->c));
+	n->m = calloc(n->mlen, sizeof(*n->m));
+	for (int i = 0; i < n->clen; i++) ii = tree(&n->c[i], input, ii, len);
+	for (int i = 0; i < n->mlen; i++) n->m[i] = input[ii++];
+	return ii;
+}
+
+int p1(struct t *n) {
+	int m = 0;
+	for (int i = 0; i < n->clen; i++) m += p1(&n->c[i]);
+	for (int i = 0; i < n->mlen; i++) m += n->m[i];
+	return m;
+}
+
+int p2(struct t *n) {
+	int v = 0;
+	for (int i = 0; i < n->mlen; i++)
+		if (!n->clen) v += n->m[i];
+		else if (n->m[i] && n->m[i] <= n->clen)
+			v += p2(&n->c[n->m[i]-1]);
+	return v;
+}
+
+int main(void) {
+	int l[1<<15], len = 0;
+	while (scanf("%d", &l[len]) == 1) len++;
+	struct t n = {0};
+	tree(&n, l, 0, len);
+	printf("%d\n%d\n", p1(&n), p2(&n));
+}
