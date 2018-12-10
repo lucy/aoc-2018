ee6f55d5671b267bbb0c14093f13f582d00f31a1
lucy <lucy@luz.lu>
2018-12-10 21:14:07 +0100

    binary search

--- 10/main.c
+++ 10/main.c
@@ -7,44 +7,60 @@
 #define letter_height 9
 
 struct p { int x, y, xv, yv; };
+struct b { int x, y, w, h; };
 
 #define min(x, y) ((x)<(y)?(x):(y))
 #define max(x, y) ((x)>(y)?(x):(y))
 #define px(p, t) (p.x+p.xv*t)
 #define py(p, t) (p.y+p.yv*t)
 
+struct b bbx(struct p *l, size_t len, int t) {
+	int xmin = INT_MAX, xmax = 0;
+	int ymin = INT_MAX, ymax = 0;
+	for (size_t i = 0; i < len; i++) {
+		xmin = min(xmin, px(l[i], t));
+		xmax = max(xmax, px(l[i], t));
+		ymin = min(ymin, py(l[i], t));
+		ymax = max(ymax, py(l[i], t));
+	}
+	return (struct b) { xmin, ymin, xmax-xmin, ymax-ymin };
+}
+
+int ph(struct p *l, size_t len, int t) {
+	int ymin = INT_MAX, ymax = 0;
+	for (size_t i = 0; i < len; i++) {
+		ymin = min(ymin, py(l[i], t));
+		ymax = max(ymax, py(l[i], t));
+	}
+	return ymax - ymin;
+}
+
 int main(void) {
-	int cap = 32, len = 0;
+	size_t cap = 32, len = 0;
 	struct p *l = malloc(cap*sizeof(*l));
 	struct p a;
 	while (scanf(format, &a.x, &a.y, &a.xv, &a.yv) == 4) {
 		if (len >= cap) l = realloc(l, (cap *= 2) * sizeof(*l));
 		l[len++] = a;
 	}
-	int t = 0;
-	while (1) {
-		int min = INT_MAX, max = 0;
-		for (int i = 0; i < len; i++) {
-			max = max(max, py(l[i], t));
-			min = min(min, py(l[i], t));
-		}
-		if ((max - min) == letter_height) break;
-		t++;
+	int t0 = 0, t1 = 1;
+	int f = ph(l, len, t0);
+	while (f > ph(l, len, t1)) t1*=2;
+	while (t0 != t1) {
+		int t = (t0+t1)/2;
+		int m = ph(l, len, t);
+		int r = ph(l, len, t+1);
+		if (r > m)
+			t1 = t;
+		else
+			t0 = t+1;
 	}
-	int wmin = INT_MAX, wmax = 0;
-	int hmin = INT_MAX, hmax = 0;
-	for (int i = 0; i < len; i++) {
-		wmin = min(wmin, px(l[i], t));
-		wmax = max(wmax, px(l[i], t));
-		hmin = min(hmin, py(l[i], t));
-		hmax = max(hmax, py(l[i], t));
-	}
-	int w = wmax - wmin + 1;
-	int h = hmax - hmin + 1;
+	struct b b = bbx(l, len, t0);
+	int w = b.w + 1, h = b.h + 1;
 	char *grid = calloc(w * h, 1);
-	for (int i = 0; i < len; i++) {
-		int x = px(l[i], t) - wmin;
-		int y = py(l[i], t) - hmin;
+	for (size_t i = 0; i < len; i++) {
+		int x = px(l[i], t1) - b.x;
+		int y = py(l[i], t1) - b.y;
 		grid[x*h+y] = 1;
 	}
 	for (int y = 0; y < h; y++) {
@@ -52,5 +68,5 @@ int main(void) {
 			putc(grid[x*h+y] ? '#' : '.', stdout);
 		putc('\n', stdout);
 	}
-	printf("%d\n", t);
+	printf("%d\n", t0);
 }
