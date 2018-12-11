7e5d2ee33ca8e828b3761dac0f6450ced3ee1e48
lucy <lucy@luz.lu>
2018-12-12 00:30:44 +0100

    readability sort of

--- 11/main.c
+++ 11/main.c
@@ -4,28 +4,31 @@
 #include <string.h>
 
 #define d 300
-#define p(x, y, s) ((((x)+10)*(y)+s)*((x)+10)/100%10-5)
 
-static inline int q(int *t, int x, int y, int s) {
-	return t[(x+s)*d+(y+s)] - t[(x  )*d+(y+s)] -
-	       t[(x+s)*d+(y  )] + t[(x  )*d+(y  )];
+static inline int power(int x, int y, int s) {
+	return ((x+10)*y+s)*(x+10)/100%10-5;
+}
+
+static inline int sum_square(int *t, int x, int y, int s) {
+	return t[(x+s)*d+y+s] - t[x*d+y+s] - t[(x+s)*d+y] + t[x*d+y];
 } 
 
-static int f(int *t, int s, int p, int *xm, int *ym) {
-	for (int m, x = 0; x < d-s; x++) for (int y = 0; y < d-s; y++)
-		if ((m = q(t, x, y, s)) > p) *xm = x, *ym = y, p = m;
-	return p;
+static int find_max_square(int *t, int s, int m, int *sm, int *xm, int *ym) {
+	for (int p, x = 0; x < d-s; x++) for (int y = 0; y < d-s; y++)
+		if ((p = sum_square(t, x, y, s)) > m)
+			*sm = s, *xm = x, *ym = y, m = p;
+	return m;
 }
 
 int main(void) {
-	int serial, *t = calloc((d+1)*(d+1), sizeof(int));
-	scanf("%d", &serial);
+	int in, *t = calloc((d+1)*(d+1), sizeof(int));
+	scanf("%d", &in);
 	for (int x = 1; x < d; x++) for (int y = 1; y < d; y++)
-		t[x*d+y] = p(x, y, serial) + t[(x-1)*d+y] + t[x*d+y-1] - t[(x-1)*d+y-1];
-	int xm, ym, sm = 3, m = f(t, sm, INT_MIN, &xm, &ym);
+		t[x*d+y] = power(x, y, in) + t[(x-1)*d+y] + t[x*d+y-1] - t[(x-1)*d+y-1];
+	int xm, ym, sm = 3;
+	int m = find_max_square(t, 3, INT_MIN, &sm, &xm, &ym);
 	printf("%d,%d\n", xm+1, ym+1);
-	for (int p, s = 1; s < d; s++)
-		if ((p = f(t, s, m, &xm, &ym)) > m)
-			sm = s, m = p;
+	for (int s = 1; s < d; s++)
+		m = find_max_square(t, s, m, &sm, &xm, &ym);
 	printf("%d,%d,%d\n", xm+1, ym+1, sm);
 }
